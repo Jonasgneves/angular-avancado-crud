cc995fa6096491dcbc694b41893a795b7e9a06ad
JonasGn <jonas_gn@hotmail.com>
2020-02-15 14:06:49 -0200

    adding category model

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,14 +1,16 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 
+import { Category } from './pages/categories/shared/category.model';
+
 export class InMemoryDatabase implements InMemoryDbService {
   createDb(reqInfo?: import('angular-in-memory-web-api').RequestInfo): {} | import('rxjs').Observable<{}> | Promise<{}> {
 
-      const categories = [
-        { id: 1 , name: 'Lazer', desciption: 'pagamento de contas de casa' },
-        { id: 1 , name: 'Saúde', desciption: 'pagamento de contas de casa' },
-        { id: 1 , name: 'Moradia', desciption: 'pagamento de contas de casa' },
-        { id: 1 , name: 'Salario', desciption: 'pagamento de contas de casa' },
-        { id: 1 , name: 'Freelas', desciption: 'pagamento de contas de casa' },
+      const categories: Category[] = [
+        { id: 1 , name: 'Lazer', description: 'Cinema,parque, praia, etc' },
+        { id: 2 , name: 'Saúde', description: 'plano de saúde e remedios' },
+        { id: 3 , name: 'Moradia', description: 'pagamento de contas de casa' },
+        { id: 4 , name: 'Salario', description: 'recebiento de salário' },
+        { id: 5 , name: 'Freelas', description: 'trabalhos como freelancer' },
       ];
       return { categories };
     }
